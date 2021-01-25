e429d8bbf1f1816d62fda60423e14bbdf2941e4e
chris32101 <48632502+chris32101@users.noreply.github.com>
2021-01-24 23:38:38 -0500

    parse function fix

--- new backend/mysite/database/models.py
+++ new backend/mysite/database/models.py
@@ -1,3 +1,5 @@
+import csv
+
 from django.db import models
 
 # Create your models here.
@@ -6,14 +8,32 @@ class Class(models.Model):
     ID = models.IntegerField()
     name = models.CharField()
     description = models.TextField()
-    HI = models.BooleanField()
-    CI = models.BooleanField()
-    DI = models.BooleanField()
-    fall = models.BooleanField()
-    spring = models.BooleanField()
-    summer = models.BooleanField()
+    HI = models.IntegerField()
+    CI = models.IntegerField()
+    DI = models.IntegerField()
+    fall = models.IntegerField()
+    spring = models.IntegerField()
+    summer = models.IntegerField()
     pathway = models.CharField()
 
-def test():
-    a = Class(prefix="", id=)
-    a.save() # --> database
+def parse():
+    with open('HASSPathways.csv', 'r') as file:
+        file_r = csv.reader(file, delimiter=',', quotechar="")
+
+        for column in file_r:
+            created = Class(
+            ID = col[1],
+            name = col[2],
+            description = col[3],
+            HI = col[4],
+            CI = col[5],
+            DI = col[6],
+            fall = col[7],
+            spring = col[8],
+            summer = col[9],
+            pathway = col[10])
+            
+            created.save() # --> database
+
+parse()
+print('Done!')
