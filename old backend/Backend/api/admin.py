cf4fcbbbaf272c58c2aa45a6d228bba1a0254066
Nishant Srivastava <20250366+nishi7409@users.noreply.github.com>
2021-03-06 23:13:29 +0530

    Removed Old FIles
    removed old files

--- old backend/Backend/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Course
-from .models import Pathway
-
-admin.site.register(Course)
-admin.site.register(Pathway)
-
-
-# Register your models here.
