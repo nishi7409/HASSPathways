a0ca2fb3872bea4d9a1fbd863cdb248f21d88efd
Nishant Srivastava <20250366+nishi7409@users.noreply.github.com>
2021-04-27 23:50:26 +0530

    removed backend from master

--- backend/database/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Pathway, Course
-
-# Register your models here.
-admin.site.register(Pathway)
-admin.site.register(Course)
--- backend/database/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('courses', views.course, name='course'),
-    path('pathways', views.pathway, name='pathway')
-]
